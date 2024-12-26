bde04cfa1894cc1a92b186a05dafaea9c0dbc881
tiedej2000 <tiedejustin1@gmail.com>
2024-12-26 21:07:51 +0100

    images path change

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const imageSet = [
     {
     id: 1,
     heading: 'Discover innovative ways to decorate',
-    backgroundUrl: '../images/desktop-image-hero-1.jpg',
+    backgroundUrl: './images/desktop-image-hero-1.jpg',
     description: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
     },
     {
         id: 2,
         heading: 'We are available all across the globe',
-        backgroundUrl: '../images/desktop-image-hero-2.jpg',
+        backgroundUrl: './images/desktop-image-hero-2.jpg',
         description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     },
     {
         id: 3,
         heading: 'Manufactured with the best materials',
-        backgroundUrl: '../images/desktop-image-hero-3.jpg',
+        backgroundUrl: './images/desktop-image-hero-3.jpg',
         description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
     }
 ]
